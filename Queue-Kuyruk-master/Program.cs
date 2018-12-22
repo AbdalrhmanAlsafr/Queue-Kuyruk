dda6929cb3759eb5a28c9381b81e4386fb2224f3
Abdalrhman Alsafr <44406952+AbdalrhmanAlsafr@users.noreply.github.com>
2018-12-22 20:34:02 +0200

    Add files via upload

---
+++ Queue-Kuyruk-master/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Queue dizi = new Queue();
+           dizi.Enqueue("1");
+            dizi.Enqueue("2");
+            dizi.Enqueue("3");
+            
+           
+
+            var son = dizi.Peek();
+            dizi.Dequeue();
+           
+         
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+    }
+}
